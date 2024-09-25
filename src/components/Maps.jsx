ae006613e8cae40e4b76347a148b671be5dd8c73
Danny27T <dannytc24@uniandes.edu.ec>
2024-09-24 19:32:11 -0500

    maps

--- src/components/Maps.jsx
+++ src/components/Maps.jsx
@@ -12,13 +12,13 @@ const Maps = () => {
       <div className="flex items-center space-x-2 text-base sm:text-lg lg:text-xl text-gray-600">
         <MapPin className="text-red-500" /> {/* Ícono de ubicación */}
         <span className="bg-gradient-to-r from-orange-500 to-orange-800 text-transparent bg-clip-text">
-          Avenida Siempre Viva 123, Latacunga, Ecuador
+          Av. Miguel Iturralde 19, Latacunga, Ecuador
         </span>
       </div>
 
       {/* Mapa de Google */}
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d5641.709605121623!2d-78.61873773709685!3d-0.9314380957792794!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x91d461a8ccaf5c47%3A0x43bb309dbf167cc3!2sConsultorio%20Odontologico%20BIODENTIS!5e0!3m2!1ses-419!2sec!4v1727151076651!5m2!1ses-419!2sec"
+        src="https://www.google.com/maps/embed?pb=!1m17!1m12!1m3!1d3989.3267560726626!2d-78.62054572503486!3d-0.8994057990918893!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m2!1m1!2zMMKwNTMnNTcuOSJTIDc4wrAzNycwNC43Ilc!5e0!3m2!1ses!2sec!4v1727199038529!5m2!1ses!2sec"
         className="w-full sm:w-3/4 lg:w-2/3 h-64 sm:h-96 lg:h-[600px] border-0"
         allowFullScreen
         loading="lazy"
